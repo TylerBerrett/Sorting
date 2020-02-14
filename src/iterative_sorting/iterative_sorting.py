9f7e111d73d6099d523838fbf0202a3d74ad678e
Tyler Berrett <btylerberrett@gmail.com>
2020-02-14 01:41:08 -0700

    add bubble search functionality

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,17 @@
 # TO-DO: Complete the selection_sort() function below
 import random
 def selection_sort(arr):
-    # loop through n-1 elements, why?
+    # goes up till the length of the array ex: if len(arr) = 12 i will only go up to 11
     for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
+        # loops through all the values of the array starting from the current index and finds the smallest value and
+        # sets the smallest_index variable to the index of that number
         for j in range(cur_index, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
 
+        # swaps places with where the the number at the current index is with the smallest number
         smallest_num = arr[smallest_index]
         position = arr[cur_index]
         arr[smallest_index] = position
@@ -23,14 +24,24 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Compare one element with the one to the right of it until I hit the end
-    for i in range(0, len(arr) - 1):
-        if arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    # keep track if something was swapped then loop again
+    # loop until no swaps happen
+    check_swap = True
+    while check_swap:
+        did_swap = 0
+        # compares the element with the one to the right of it and if the element is larger than the one on the right
+        # then swap elements and add to did_swap to let the while loop know that we are not done yet
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                did_swap += 1
+                
+        # notifies the while loop to stop if no swaps were preformed
+        if did_swap == 0:
+            check_swap = False
 
     return arr
 
-
-print(bubble_sort([0, 3, 14, 2, 123]))
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     return arr

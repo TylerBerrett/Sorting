ef280c073ec337cb38b51e73d2bc5f8d62d6248f
Tyler Berrett <btylerberrett@gmail.com>
2020-02-18 15:50:30 -0700

    add filtering left and right lists

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,24 +3,37 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
+    
+
 
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 # divide in half until you have subarrays of length 1
+# sort list
 # merge sorted lists together
-# index 1 = right, 0 = left
 def merge_sort(arr):
     # TO-DO
     if len(arr) > 1:
+        
+        if arr[0] > arr[1]:
+            arr[0], arr[1] = arr[1], arr[0]
+
+        if len(arr) == 3:
+            if arr[1] > arr[2]:
+                arr[1], arr[2] = arr[2], arr[1]
+                if arr[1] < arr[0]:
+                    arr[0], arr[1] = arr[1], arr[0]
+
         half = len(arr) / 2
-        return merge_sort(arr[0: int(half)]), merge_sort(arr[int(half): len(arr)])
+        return merge(merge_sort(arr[:int(half)]), merge_sort(arr[int(half): len(arr)]))
+
     else:
         return arr
 
 
-test = [1, 4, 12, 89, 46, 12, 14, 56, 98, 123]
+test = [4, 1, 53, 89, 46, 33, 14, 56, 98, 123]
 
 print(merge_sort(test))
 
